b2d5b4af6661279df225b0c253587251011643d5
Lowell Roxas <lkfroxas@gmail.com>
2018-02-04 16:12:38 -0800

    Add init script for masterwork materials.

---
+++ db/scripts/dcscript_InitMaterialMasterwork.js
@@ -0,0 +1,50 @@
+db.materials.insertMany([
+  {
+    name: "Fade-Touched Bloodstone",
+    type: "Masterwork",
+    tier: 2,
+    properties: [
+      { type: "Masterwork", property: "Damage - Nearby Enemies" }
+    ]
+  },
+  {
+    name: "Fade-Touched Obsidian",
+    type: "Masterwork",
+    tier: 2,
+    properties: [
+      { type: "Masterwork", property: "Guard" }
+    ]
+  },
+  {
+    name: "Fade-Touched Pyrophite",
+    type: "Masterwork",
+    tier: 2,
+    properties: [
+      { type: "Masterwork", property: "Damage - Nearby Enemies" }
+    ]
+  },
+  {
+    name: "Fade-Touched Ring Velvet",
+    type: "Masterwork",
+    tier: 2,
+    properties: [
+      { type: "Masterwork", property: "Hidden Blades" }
+    ]
+  },
+  {
+    name: "Fade-Touched Plush Fustian Velvet",
+    type: "Masterwork",
+    tier: 3,
+    properties: [
+      { type: "Masterwork", property: "Hidden Blades" }
+    ]
+  },
+  {
+    name: "Fade-Touched Silverite",
+    type: "Masterwork",
+    tier: 3,
+    properties: [
+      { type: "Masterwork", property: "Guard" }
+    ]
+  }
+]);
