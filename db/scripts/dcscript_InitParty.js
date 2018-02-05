ff1a5ee5a90e3c0ea55b4a8bb82e4bd10d2446e2
Lowell Roxas <lkfroxas@gmail.com>
2018-02-04 19:38:05 -0800

    Add first 3 party members to init script.

---
+++ db/scripts/dcscript_InitParty.js
@@ -0,0 +1,75 @@
+db.party.insertMany([
+  {
+    name: "Luthais",
+    gear: [
+      {
+        schematic: "Superior Battlemage Armor",
+        names: [
+          { tier: 1, name: "Luthais's Armor" },
+          { tier: 2, name: "Runic Armor" },
+          { tier: 3, name: "Glyphic Armor" },
+          { tier: 4, name: "Archmage Armor" }
+        ]
+      },
+      {
+        schematic: "Encore",
+        names: [
+          { tier: 1, name: "Spark" },
+          { tier: 2, name: "Bolt" },
+          { tier: 3, name: "Lighting Lord" },
+          { tier: 4, name: "Master of Storms" }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Rhaiel",
+    gear: [
+      {
+        schematic: "Superior Battlemaster Armor",
+        names: [
+          { tier: 1, name: "Rhaiel's Armor" },
+          { tier: 2, name: "Dalish Armor" },
+          { tier: 3, name: "Arlathan Armor" },
+          { tier: 4, name: "Andruil's Armor" }
+        ]
+      },
+      {
+        schematic: "Prismatic Greataxe",
+        names: [
+          { tier: 1, name: "Trial" },
+          { tier: 2, name: "Verdict" },
+          { tier: 3, name: "Sentence" },
+          { tier: 4, name: "Execution" }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Hawthorne",
+    gear: [
+      {
+        schematic: "Superior Prowler Armor",
+        names: [
+          { tier: 1, name: "Hawthorne's Armor" },
+          { tier: 2, name: "Carta Armor" },
+          { tier: 3, name: "Deshyr Armor" },
+          { tier: 4, name: "Paragon Armor" }
+        ]
+      },
+      {
+        schematic: "Stone Stalker's Blade",
+        names: [
+          { tier: 1, name: "Cut" },
+          { tier: 1, name: "Take" },
+          { tier: 2, name: "Beraht's Finger" },
+          { tier: 2, name: "Jarvia's Toe" },
+          { tier: 3, name: "Hot Poker" },
+          { tier: 3, name: "Tingly Point" },
+          { tier: 4, name: "Serpent of Avarice" },
+          { tier: 4, name: "Song of Autumn" },
+        ]
+      }
+    ]
+  }
+]);
