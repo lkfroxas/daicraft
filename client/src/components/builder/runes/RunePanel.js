b757651b668a72a47bf95d1f3725e8bf371b596a
Lowell Roxas <lkfroxas@gmail.com>
2018-03-03 01:24:48 -0800

    Rework Builder layout.

--- client/src/components/builder/runes/RunePanel.js
+++ client/src/components/builder/runes/RunePanel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TierSelect from '../../shared/TierSelect.js';
 import RuneSelect from './RuneSelect.js';
 import CraftActions from '../../../actions/CraftActions.js';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Panel, Grid, Row, Col } from 'react-bootstrap';
 
 class RunePanel extends React.Component {
   state = { tier: "" };
@@ -33,22 +33,26 @@ class RunePanel extends React.Component {
     );
 
     return (
-      <Grid fluid>
-        <Row>
-          <Col lg={2}>
-            Rune
-          </Col>
-          <Col lg={2}>
-            <TierSelect
-              onChange={this.handleTierChange.bind(this)}
-              tiers={["2", "3"]}
-            />
-          </Col>
-          <Col lg={8}>
-            {rune}
-          </Col>
-        </Row>
-      </Grid>
+      <Panel>
+        <Panel.Heading>
+          Select Rune
+        </Panel.Heading>
+        <Panel.Body>
+          <Grid fluid>
+            <Row>
+              <Col lg={2}>
+                <TierSelect
+                  onChange={this.handleTierChange.bind(this)}
+                  tiers={["2", "3"]}
+                />
+              </Col>
+              <Col lg={10}>
+                {rune}
+              </Col>
+            </Row>
+          </Grid>
+        </Panel.Body>
+      </Panel>
     );
   }
 }
