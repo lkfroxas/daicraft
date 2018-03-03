a4a7a96d23f6e3cca8f18ea9e3b83a2504e44bbe
Lowell Roxas <lkfroxas@gmail.com>
2018-03-02 22:58:39 -0800

    Convert Build section into a Bootstrap panel.

--- client/src/components/builder/BuilderSection.js
+++ client/src/components/builder/BuilderSection.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import SchematicPick from './SchematicPick.js';
 import SchematicAssemble from './SchematicAssemble.js';
+import { Panel } from 'react-bootstrap';
 
 class BuilderSection extends React.Component {
+  state = { open: true };
+
   render() {
     const schematicAssemble = (this.props.schematic.name === "") ? "" : (
       <SchematicAssemble
@@ -12,11 +15,21 @@ class BuilderSection extends React.Component {
 
     return (
       <section>
-        <h3>Build Item</h3>
-        <SchematicPick
-          schematic={this.props.schematic}
-        />
-        {schematicAssemble}
+        <Panel defaultExpanded>
+          <Panel.Heading>
+            <Panel.Title toggle>
+              <h3>Build Item</h3>
+            </Panel.Title>
+          </Panel.Heading>
+          <Panel.Collapse>
+            <Panel.Body>
+              <SchematicPick
+                schematic={this.props.schematic}
+              />
+              {schematicAssemble}
+            </Panel.Body>
+          </Panel.Collapse>
+        </Panel>
       </section>
     );
   }
