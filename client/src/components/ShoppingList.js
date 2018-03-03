9ad20bc2fe6ae20621f3291de7de7cabd138e0fd
Lowell Roxas <lkfroxas@gmail.com>
2018-03-02 23:09:57 -0800

    Convert Shopping List into a Bootstrap panel.

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Panel } from 'react-bootstrap';
 
 function ShoppingList(props) {
   const listArray = Object.entries(props.shoppingList);
@@ -13,16 +14,26 @@ function ShoppingList(props) {
 
   return (
     <section>
-      <h3>Shopping List</h3>
-      <table>
-        <tbody>
-          <tr>
-            <th>Material</th>
-            <th>Quantity</th>
-          </tr>
-          {materialsList}
-        </tbody>
-      </table>
+      <Panel defaultExpanded>
+        <Panel.Heading>
+          <Panel.Title toggle>
+            <h3>Shopping List</h3>
+          </Panel.Title>
+        </Panel.Heading>
+        <Panel.Collapse>
+          <Panel.Body>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Material</th>
+                  <th>Quantity</th>
+                </tr>
+                {materialsList}
+              </tbody>
+            </table>
+          </Panel.Body>
+        </Panel.Collapse>
+      </Panel>
     </section>
   );
 }
