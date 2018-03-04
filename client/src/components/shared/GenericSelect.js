a6dba6b6f2aac5ee2a0519eccc4595b62967c58e
Lowell Roxas <lkfroxas@gmail.com>
2018-03-03 20:25:05 -0800

    Debug selects.

--- client/src/components/shared/GenericSelect.js
+++ client/src/components/shared/GenericSelect.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import SelectOption from './SelectOption.js';
-import Select from './Select.js';
+import { FormControl } from 'react-bootstrap';
+import { isEqual } from 'lodash';
 
-let GenericSelect = function(props) {
-  const options = props.options.map(option => {
+class GenericSelect extends React.Component {
+  state = { selectedValue: "" }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!isEqual(this.props.options, prevProps.options)) {
+      this.setState({ selectedValue: "" });
+    }
+  }
+
+  handleSelectedValueChanged(event) {
+    this.setState({ selectedValue: event.target.value });
+    this.props.onChange(event);
+  }
+
+  render() {
+    const options = this.props.options.map(option => {
+      return (
+        <SelectOption
+          key={option.value}
+          value={option.value}
+          name={option.name}
+        />
+      );
+    });
+
     return (
-      <SelectOption
-        key={option.value}
-        value={option.value}
-        name={option.name}
-      />
+      <FormControl
+        onChange={this.handleSelectedValueChanged.bind(this)}
+        componentClass="select"
+        value={this.state.selectedValue}>
+        <option value="">{this.props.name}</option>
+        {options}
+      </FormControl>
     );
-  });
-
-  return (
-    <Select
-      onChange={props.onChange}
-      blankOptionName={props.name}
-      options={options}
-    />
-  );
+  }
 }
 
 export default GenericSelect;
